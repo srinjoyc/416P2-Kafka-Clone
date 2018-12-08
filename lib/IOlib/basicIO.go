322befa1f0ef98d1d926fefd5ca9a444719d91f8
BestOreo <geshuaiqi001@gmail.com>
2018-12-07 18:10:09 -0800

    zero-copy policy

--- lib/IOlib/basicIO.go
+++ lib/IOlib/basicIO.go
@@ -1,7 +1,11 @@
 package IOlib
+
 import (
+	"io"
 	"io/ioutil"
 	"log"
+	"net"
+	"os"
 )
 
 /* readFileByte
@@ -10,7 +14,7 @@ import (
  * @para: filePath: relative url of file
  * @Return: []byte
  */
- func ReadFileByte(filePath string) []byte {
+func ReadFileByte(filePath string) []byte {
 	data, err := ioutil.ReadFile(filePath)
 	if err != nil {
 		log.Fatal(err)
@@ -18,4 +22,13 @@ import (
 	return data
 }
 
-
+// Sendfile: Zero copy
+// Sendfile sends count bytes from f to remote a TCP connection.
+// f offset is always relative to the current offset.
+func Sendfile(conn *net.TCPConn, filename string, start int64, length int64) (n int64, err error) {
+	f, err := os.Open(filename)
+	f.Seek(start, 0)
+	lr := &io.LimitedReader{N: length, R: f}
+	n, err = conn.ReadFrom(lr)
+	return
+}
